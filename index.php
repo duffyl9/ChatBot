ba750e8b4455a12b5e9eabcf0e5ffe913e562c9a
nualand <nidhomnu@tcd.ie>
2018-11-08 17:50:05 +0000

    home page

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+  <head id = "hi">
+    <link href="/static/css/main.css" type="text/css" rel="stylesheet">
+
+    <title>TCD Book a Room</title>
+  </head>
+
+  <body>
+    <div class = "top-banner">
+      <div id = "logo">
+        <img src="/static/img/trinity-logo.png" height="80">
+      </div>
+      <h1 id="TCD-Book"> TCD Book a Room </h1>
+     </div>
+
+    <div class = "About">
+      <h3>About TCD Book a Room</h3>
+      <p> Welcome to TCD Book a Room.
+          <br></br>
+          This site enables both
+          Students and Staff of TCD to easily book a room on campus.
+          Our chat bot provides an efficient and user frieendly service.
+          <br></br>
+          If you are a Trinity College student
+          please <em><a href ="./login.php">login</a></em> with your college login details
+      </p>
+    </div>
+    <div>
+
+  </body>
+
+</html>
