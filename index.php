de6e93f71e9540ea05d5c0c5b341359b6b0219e6
nualand <nidhomnu@tcd.ie>
2018-11-11 19:50:04 +0000

    home/login page

--- index.php
+++ index.php
@@ -4,34 +4,57 @@
 <!DOCTYPE html>
 <html>
 
-  <head id = "hi">
-    <link href="/static/css/main.css" type="text/css" rel="stylesheet">
-
-    <title>TCD Book a Room</title>
+  <head >
+    <link href="static/css/main.css" type="text/css" rel="stylesheet">
+    <link href="static/css/register.css" type="text/css" rel="stylesheet">
   </head>
 
   <body>
     <div class = "top-banner">
-      <div id = "logo">
-        <img src="/static/img/trinity-logo.png" height="80">
-      </div>
-      <h1 id="TCD-Book"> TCD Book a Room </h1>
+        <img src="static/img/trinity-logo.png" height = 65% >
+        <h1 id = "title" >TCD Book a Room </h1> 
+
      </div>
+    <div class = "back">
+        <div class = "About">
+        <h3>About TCD Book a Room</h3>
 
-    <div class = "About">
-      <h3>About TCD Book a Room</h3>
-      <p> Welcome to TCD Book a Room.
-          <br></br>
-          This site enables both
-          Students and Staff of TCD to easily book a room on campus.
-          Our chat bot provides an efficient and user frieendly service.
-          <br></br>
-          If you are a Trinity College student
-          please <em><a href ="./login.php">login</a></em> with your college login details
-      </p>
+          <div id = "welcome-text">
+            <p> Welcome to TCD Book a Room.
+            This site enables both
+            Students and Staff of TCD to easily book a room on campus.
+             Our chat bot provides an efficient and user frieendly service.
+            If you are a Trinity College student
+            please with your college login details
+            </p>
+        </div>
+
     </div>
-    <div>
+      <div class="register">
+      	<form action="sign-in.php" method="post">
+      		<div>
+      			<p><!--session message will go here --></p>
+      		</div>
+      		<div class="sign-up-elem">
+      			<div class="label">
+      				Email:
+      			</div>
+      			<input class="input" type="text" name="email" required autocomplete="off"> <br />
+      		</div>
+      		<div class="sign-up-elem">
+      			<div class="label">
+      				Password:
+      			</div>
+      			<input class="input" type="password" name="password" required autocomplete="off"> <br />
+      		</div>
 
+      		<div class="sign-up-elem">
+      			<button class="btn" type="text" name="forgotpassword" >Login</button>
+      		<div class = "sign-up-elem">
+      			<p> Login with your college account </p>
+		</div>
+    </div>
+  </div>
   </body>
 
 </html>
